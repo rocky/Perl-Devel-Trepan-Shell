9c41e7c98eeb0499f52fd48fed76456ebdef801c
Rocky Bernstein <rocky@cpan.org>
2012-01-06 21:53:56 -0500

    Trepan.pm: rough cut at adding %debug to debug into an expression. Biuld.PL: add dependencies.

---
+++ lib/Devel/REPL/Plugin/Trepan.pm
@@ -0,0 +1,39 @@
+package Devel::REPL::Plugin::Trepan;
+print __FILE__, " here\n";
+
+use Devel::REPL::Plugin;
+use Enbugger 'trepan';
+use namespace::clean -except => [ 'meta' ];
+
+has 'history' => (
+   isa => 'ArrayRef', is => 'rw', required => 1, lazy => 1,
+   default => sub { [] }
+);
+
+around 'read' => sub {
+   my $orig = shift;
+   my ($self, @args) = @_;
+   my $line = $self->$orig(@args);
+   if (defined $line) {
+       if ($line =~ m/^%debug\s+(.*)$/) {
+	   my $eval_code = $1;
+	   eval "
+Enbugger->stop;  # newline is nice to have in showing code
+$eval_code;
+\$DB::signal = \$DB::single = \$DB::trace = 0;
+"
+       }
+   }
+   return $line;
+};
+
+1;
+
+__END__
+
+=head1 NAME
+
+Devel::REPL::Plugin::TrepanShell - Add '%' commands to call back Trepan in a Devel::REPL shell invoked from Devel::Trepan
+
+=cut
+
