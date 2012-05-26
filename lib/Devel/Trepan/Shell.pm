a2661172ab929ef842bb9fc90b001dd28e13d678
Rocky Bernstein <rocky@cpan.org>
2012-05-26 09:52:42 -0400

    Build.PL - we don't do Psh here any more.

--- lib/Devel/Trepan/Shell.pm
+++ lib/Devel/Trepan/Shell.pm
@@ -2,7 +2,7 @@
 # Copyright (C) 2012 Rocky Bernstein <rocky@cpan.org>
 # Documentation is at the __END__
 package Devel::Trepan::Shell;
-use version; $VERSION = '1.4';
+use version; $VERSION = '1.4_001';
 "All of the real action is in Devel::Trepan::CmdProcessor::Command::Shell.pm";
 __END__
 
